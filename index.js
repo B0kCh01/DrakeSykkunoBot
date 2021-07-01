06be2b512457f7b74d8d4c9fbfe59feb70c2143e
b0kch01 <nath49ers@gmail.com>
2021-07-01 13:17:53 -0700

    Different way to handle errors + some UI fixes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
-const fetch = require('node-fetch');
-var request = require('request');
-var gm = require('gm');
+const fetch = require("node-fetch");
+var request = require("request");
+var gm = require("gm");
 
 let settings = { method: "Get" };
 
@@ -29,7 +29,7 @@ let songs = [
     "Marvin's Room",
     "$help",
     "Hotline Bling",
-    "$help"
+    "$help",
 ];
 
 let captions = [
@@ -46,132 +46,139 @@ let captions = [
     "Sykkuno without makeup",
     "Sykkuno is ending stream",
     "Joined the sykkcult",
-    "A lovely day, isn't it Sykkuno?", 
-    "Do i have to Sykkuno?", 
-    "Can I help you Sykkuno?", 
-    "How are things going Sykkuno?", 
-    "Any thing else Sykkuno?", 
-    "Are you kidding Sykkuno?", 
-    "Are you sure Sykkuno?", 
-    "Do you understand me Sykkuno?", 
-    "Are you done Sykkuno?", 
-    "Can I ask you something Sykkuno?", 
-    "Can you please repeat that Sykkuno?", 
-    "Did you get it Sykkuno?", 
-    "Do you need anything Sykkuno?", 
-    "How are you Sykkuno?", 
-    "How do you feel Sykkuno?", 
-    "How much is it Sykkuno?", 
-    "How old are you Sykkuno?", 
-    "How was your weekend Sykkuno?", 
-    "Is all good Sykkuno?", 
-    "Is everything OK Sykkuno?", 
-    "What are you doing Sykkuno?", 
-    "What are you talking about Sykkuno?", 
-    "What are you up to Sykkuno?", 
-    "What are your hobbies Sykkuno?", 
-    "What did you say Sykkuno?", 
-    "What do you need Sykkuno?", 
-    "What do you think Sykkuno?", 
-    "What do you want to do Sykkuno?", 
-    "What do you want Sykkuno?", 
-    "What's the weather like Sykkuno?", 
-    "What's your e-mail address Sykkuno?", 
-    "What is your job Sykkuno?", 
-    "What's your name Sykkuno?", 
-    "What's your phone\nnumber Sykkuno?", 
-    "What is going on Sykkuno?", 
-    "When is the train leaving Sykkuno?", 
-    "How can I go to the\ntown centre Sykkuno?", 
-    "Where are you from Sykkuno?", 
-    "Where are you going Sykkuno?", 
-    "Where are you Sykkuno?", 
-    "Where did you get it Sykkuno?", 
-    "Where do you live Sykkuno?", 
-    "Are you coming with me Sykkuno?", 
-    "How long will you stay Sykkuno?", 
-    "I agree Sykkuno.", 
-    "I'm at home Sykkuno.", 
-    "It's on the tip of\nmy tongue Sykkuno.", 
-    "It's ok Sykkuno.", 
-    "It really takes time Sykkuno.", 
-    "No, I don't want Sykkuno.", 
-    "See you Sykkuno.", 
-    "See you next time Sykkuno.", 
-    "Allow me Sykkuno.", 
-    "Any day will do Sykkuno.", 
-    "Be calm Sykkuno.", 
-    "Be careful Sykkuno!", 
-    "Be quiet Sykkuno!", 
-    "Cheer up Sykkuno!", 
-    "Come on Sykkuno.", 
-    "Don't be ridicolus Sykkuno!", 
-    "Don't be so childish Sykkuno!", 
-    "Don't move Sykkuno!", 
-    "Don't worry Sykkuno.", 
-    "Enjoy yourself Sykkuno.", 
-    "Follow me Sykkuno.", 
-    "Forgive me Sykkuno.", 
-    "Forget it Sykkuno.", 
-    "God bless you Sykkuno.", 
-    "It's very thoughtful\nof you Sykkuno.", 
-    "It's up to you Sykkuno.", 
-    "It's none of you\nbusiness Sykkuno.", 
-    "Slow down Sykkuno.", 
-    "Stop making such a noice Sykkuno.", 
-    "You are going too fast Sykkuno.", 
-    "Come with me Sykkuno.", 
-    "Good afternoon Sykkuno.", 
-    "Good morning Sykkuno.", 
-    "Good night Sykkuno.", 
-    "Have a good trip Sykkuno.", 
-    "Have a good weekend Sykkuno.", 
-    "I admire you Sykkuno.", 
-    "I apologize Sykkuno.", 
-    "I can't wait Sykkuno.", 
-    "I don't have time Sykkuno.", 
-    "I got it Sykkuno.", 
-    "I hate you Sykkuno!", 
-    "I hope so Sykkuno.", 
-    "I knew it Sykkuno.", 
-    "I love you Sykkuno.", 
-    "I would love to Sykkuno.", 
-    "I am busy Sykkuno.", 
-    "I am tired Sykkuno.", 
-    "I don't agree Sykkuno.", 
-    "You are wasting my time Sykkuno.", 
-    "I feel much better Sykkuno.", 
-    "They like each other Sykkuno.", 
-    "I'm sorry Sykkuno.", 
-    "I'm good Sykkuno.", 
-    "It doesn't matter Sykkuno.", 
-    "Join me Sykkuno.", 
-    "Let's catch up Sykkuno!", 
-    "Let's do it Sykkuno.", 
-    "Nice to meet you Sykkuno.", 
-    "Not yet Sykkuno.", 
-    "Talk to you tomorrow Sykkuno.", 
-    "Thank you very much Sykkuno.", 
-    "You turn Sykkuno."
+    "A lovely day, isn't it Sykkuno?",
+    "Do i have to Sykkuno?",
+    "Can I help you Sykkuno?",
+    "How are things going Sykkuno?",
+    "Any thing else Sykkuno?",
+    "Are you kidding Sykkuno?",
+    "Are you sure Sykkuno?",
+    "Do you understand me Sykkuno?",
+    "Are you done Sykkuno?",
+    "Can I ask you something Sykkuno?",
+    "Can you please repeat that Sykkuno?",
+    "Did you get it Sykkuno?",
+    "Do you need anything Sykkuno?",
+    "How are you Sykkuno?",
+    "How do you feel Sykkuno?",
+    "How much is it Sykkuno?",
+    "How old are you Sykkuno?",
+    "How was your weekend Sykkuno?",
+    "Is all good Sykkuno?",
+    "Is everything OK Sykkuno?",
+    "What are you doing Sykkuno?",
+    "What are you talking about Sykkuno?",
+    "What are you up to Sykkuno?",
+    "What are your hobbies Sykkuno?",
+    "What did you say Sykkuno?",
+    "What do you need Sykkuno?",
+    "What do you think Sykkuno?",
+    "What do you want to do Sykkuno?",
+    "What do you want Sykkuno?",
+    "What's the weather like Sykkuno?",
+    "What's your e-mail address Sykkuno?",
+    "What is your job Sykkuno?",
+    "What's your name Sykkuno?",
+    "What's your phone\nnumber Sykkuno?",
+    "What is going on Sykkuno?",
+    "When is the train leaving Sykkuno?",
+    "How can I go to the\ntown centre Sykkuno?",
+    "Where are you from Sykkuno?",
+    "Where are you going Sykkuno?",
+    "Where are you Sykkuno?",
+    "Where did you get it Sykkuno?",
+    "Where do you live Sykkuno?",
+    "Are you coming with me Sykkuno?",
+    "How long will you stay Sykkuno?",
+    "I agree Sykkuno.",
+    "I'm at home Sykkuno.",
+    "It's on the tip of\nmy tongue Sykkuno.",
+    "It's ok Sykkuno.",
+    "It really takes time Sykkuno.",
+    "No, I don't want Sykkuno.",
+    "See you Sykkuno.",
+    "See you next time Sykkuno.",
+    "Allow me Sykkuno.",
+    "Any day will do Sykkuno.",
+    "Be calm Sykkuno.",
+    "Be careful Sykkuno!",
+    "Be quiet Sykkuno!",
+    "Cheer up Sykkuno!",
+    "Come on Sykkuno.",
+    "Don't be ridicolus Sykkuno!",
+    "Don't be so childish Sykkuno!",
+    "Don't move Sykkuno!",
+    "Don't worry Sykkuno.",
+    "Enjoy yourself Sykkuno.",
+    "Follow me Sykkuno.",
+    "Forgive me Sykkuno.",
+    "Forget it Sykkuno.",
+    "God bless you Sykkuno.",
+    "It's very thoughtful\nof you Sykkuno.",
+    "It's up to you Sykkuno.",
+    "It's none of you\nbusiness Sykkuno.",
+    "Slow down Sykkuno.",
+    "Stop making such a noice Sykkuno.",
+    "You are going too fast Sykkuno.",
+    "Come with me Sykkuno.",
+    "Good afternoon Sykkuno.",
+    "Good morning Sykkuno.",
+    "Good night Sykkuno.",
+    "Have a good trip Sykkuno.",
+    "Have a good weekend Sykkuno.",
+    "I admire you Sykkuno.",
+    "I apologize Sykkuno.",
+    "I can't wait Sykkuno.",
+    "I don't have time Sykkuno.",
+    "I got it Sykkuno.",
+    "I hate you Sykkuno!",
+    "I hope so Sykkuno.",
+    "I knew it Sykkuno.",
+    "I love you Sykkuno.",
+    "I would love to Sykkuno.",
+    "I am busy Sykkuno.",
+    "I am tired Sykkuno.",
+    "I don't agree Sykkuno.",
+    "You are wasting my time Sykkuno.",
+    "I feel much better Sykkuno.",
+    "They like each other Sykkuno.",
+    "I'm sorry Sykkuno.",
+    "I'm good Sykkuno.",
+    "It doesn't matter Sykkuno.",
+    "Join me Sykkuno.",
+    "Let's catch up Sykkuno!",
+    "Let's do it Sykkuno.",
+    "Nice to meet you Sykkuno.",
+    "Not yet Sykkuno.",
+    "Talk to you tomorrow Sykkuno.",
+    "Thank you very much Sykkuno.",
+    "You turn Sykkuno.",
 ];
 
 client.on("ready", () => {
     console.log("===============================");
-    console.log(`Bot loaded as ${client.user.tag}!`);
-    console.log("===============================\n");
+    console.log(`[✓] Bot loaded as ${client.user.tag}!\n`);
 
     let i = 0;
 
     setInterval(function () {
         client.user.setActivity(songs[i % songs.length], {
-            type: "LISTENING"
+            type: "LISTENING",
         });
 
         i++;
     }, 10000);
 });
 
-client.on("message", msg => {
+var errors = [];
+process.on("unhandledRejection", (error) => {
+    let errorMSG = `[${getTimestamp()}] Failed: ` + error.message;
+    errors.push(errorMSG.replace("Missing Access", "I don't have enough permissions in that channel."));
+
+    console.error(`[${getTimestamp()}]`, error);
+});
+
+client.on("message", (msg) => {
     if (msg.author.bot) return;
 
     if (msg.content.startsWith("$")) {
@@ -184,47 +191,47 @@ client.on("message", msg => {
             MEME COMMAND
         */
 
-        if (main === "meme") {    
+        if (main === "meme") {
             if (args[0]) {
                 sendMeme(msg, true, {
-                    name: args[0]
+                    name: args[0],
                 });
             } else {
                 sendMeme(msg, true);
             }
-        }
-
-        /*
-            CUSTOM COMMAND
-        */
-
-        else if (main ===  "custom") {
+        } else if (main === "custom") {
+            /*
+                CUSTOM COMMAND
+            */
             let message = args[0] || "";
             sendMeme(msg, true, {
-                custom: message
+                custom: message,
             });
-        }
-            
-        /*
-            HELP COMMAND
-        */
-            
-        else if (main === "help") {
-            react(msg);
-            
+        } else if (main === "help") {
+            /*
+                HELP COMMAND
+            */
+            msg.react("👍");
+
             /*jshint multistr: true */
             let message =
                 "** DRAKE x SYKKUNO BOT**" +
                 "```I generate drake-sykkuno memes for you```\n" +
-                "`$meme` or include \"sykkuno\" in your message >> Generates a meme for you\n" +
+                '`$meme` or include "sykkuno" in your message >> Generates a meme for you\n' +
                 "`$meme Some Name` >> Uses the name or phrase in the caption\n" +
                 "`$custom Some Caption` >> Creates a drake gif with a random caption (don't make it too long)\n" +
+                "`$broke` >> Find out why the bot is not working anymore. No response? LMK.\n" +
                 "`$help` >> Bot commands list\n" +
-                "\_\_\_\_\_\_\_\n" +
-                "Built with ♥ by @b0kch01   --   Idea by Henry"
-            ;
-
+                "_______\n" +
+                "Built with ♥ by @b0kch01   --   Idea by Henry";
             msg.channel.send(message);
+        } else if (main === "broke") {
+            client.users.fetch(msg.author.id, false).then((user) => {
+                let cached = errors.reverse();
+                let message = (cached[0] || "---") + "\n" + (cached[1] || "---") + "\n" + (cached[2] || "---");
+
+                user.send("**What went wrong (last three errors):**\n" + message);
+            });
         }
     } else {
         if (
@@ -237,7 +244,7 @@ client.on("message", msg => {
 });
 
 function sendMeme(msg, repeatOnError, customMessage) {
-    react(msg);
+    msg.react("♥");
 
     const pos = Math.floor(Math.random() * 20);
     let caption = randVal(captions);
@@ -245,41 +252,52 @@ function sendMeme(msg, repeatOnError, customMessage) {
     if (customMessage) {
         if (customMessage.custom) {
             caption = customMessage.custom;
-        }
-        else if (customMessage.name) {
+        } else if (customMessage.name) {
             caption = caption.replace("Sykkuno", customMessage.name);
         }
     }
-    
-    fetch(`https://g.tenor.com/v1/search?q=drake&key=JURBTZ9GDIWX&limit=50&media_filter=minimal&pos=${pos}`, settings)
-        .then(data => data.json())
-        .then(output => {
+
+    fetch(
+        `https://g.tenor.com/v1/search?q=drake&key=JURBTZ9GDIWX&limit=50&media_filter=minimal&pos=${pos}`,
+        settings
+    )
+        .then((data) => data.json())
+        .then((output) => {
             let gifs = output.results;
-            console.log(`[${getTimestamp()}] Searched for position ${pos} and got ${gifs.length} results`);
-            
-            
+            console.log(
+                `[${getTimestamp()}] Searched for position ${pos} and got ${
+                    gifs.length
+                } results`
+            );
+
             let drake = randVal(gifs);
             while (drake.media[0].gif.size > 8000000) {
                 drake = randVal(gifs);
             }
-            
+
             let url = drake.media[0].gif.url;
 
             gm(request(url))
                 .stroke("#000000")
-                .fill('#ffffff')
+                .fill("#ffffff")
                 .font("./impact.ttf", 30)
                 .dither(false)
-                .drawText(0, 40, caption, 'South')
+                .drawText(0, 40, caption, "South")
                 .noProfile()
                 .bitdepth(8)
                 .write("./gifs/output.gif", function (error) {
                     if (!error) {
-                        msg.channel.send("", { files: ["./gifs/output.gif"] })
-                            .catch(error => {
+                        msg.channel
+                            .send("", { files: ["./gifs/output.gif"] })
+                            .catch((error) => {
+                                console.log("error");
                                 errorMsg(msg, error);
                                 if (repeatOnError) {
-                                    msg.reply("Let me try again...");
+                                    msg.reply("Let me try again...").catch(
+                                        (error) => {
+                                            dmPermError();
+                                        }
+                                    );
                                     sendMeme(msg, false, customMessage);
                                 }
                             });
@@ -288,33 +306,41 @@ function sendMeme(msg, repeatOnError, customMessage) {
                     }
                 });
         })
-        .catch(error => {
+        .catch((error) => {
             errorMsg(msg, error);
         });
 }
 
 function errorMsg(msg, error) {
-    msg.reply(`They **prayed** for my downfall: \n\`\`\`json\n${error}\`\`\``);
+    msg.react("❌").catch();
+    msg.reply(
+        `They **prayed** for my downfall: \n\`\`\`json\n${error}\`\`\``
+    ).catch((error) => {
+        dmPermError();
+    });
 }
 
 function getTimestamp() {
-    const today = new Date();
-    const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const now = new Date();
+    const date =
+        now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate();
+    const time =
+        now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
     return date + " " + time;
 }
 
 function randVal(list) {
-    return list[Math.floor((Math.random() * list.length))];
+    return list[Math.floor(Math.random() * list.length)];
 }
 
-function react(msg) {
-    msg.react("♥")
-        .catch(() => {
-            msg.reply("I don't have permission to react to messages! 🙏");
-        });
+function dmPermError() {
+    client.users.fetch("293903980935774208", false).then((user) => {
+        user.send(
+            "I don't think I had the proper permissions (react, send, attach) in that channel :("
+        ).catch();
+    });
 }
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 client.login(process.env.TOKEN);
